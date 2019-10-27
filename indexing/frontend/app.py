c17af002be491e95ffd8cea27d8dec171151d7bb
ericstarmars1 <lyic@163.com>
2019-10-27 22:53:29 +0800

    Implement a simple frontend to verify the search function

---
+++ indexing/frontend/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template, redirect, url_for
+from indexing.backend.PlaceSearch import PlaceSearch
+from indexing.backend.PlaceShow import PlaceShow
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        queryStr = request.form.get('searchbox')
+        print("Query string is " + str(queryStr))
+        doclist = plcSearch.search(queryStr)
+        resultlist = [plcShow.get_item(docItem[0]) for docItem in doclist]
+        return render_template('result.html', querystr=queryStr, resultlist=resultlist, resultlen=len(resultlist))
+        #return "query string is " + str(queryStr)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    """First we do initilization"""
+    oriDocFile = "../backend/PlacesResults.json"
+    indexFile = "../backend/PlacesIndex.json"
+    docFile = "../backend/PlacesDoc.json"
+    plcSearch = PlaceSearch(indexFile, docFile)
+    plcSearch.load()
+    plcShow = PlaceShow(oriDocFile)
+    plcShow.load()
+
+    app.run(debug=True, port=5000)

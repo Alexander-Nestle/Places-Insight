f88d719f5e7e57a0dff9d64bf6c77306710648dd
Alexander Nestle <alexander.nestle32@gmail.com>
2019-12-03 21:40:09 +0900

    Topic Badges Added

--- indexing/app.py
+++ indexing/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for
 from backend.PlaceSearch import SearchRank, PlaceShow
 from backend.Config import Config
 from backend.DataBase import DBManager
+from backend.topicModel.TopicModelService import TopicModelService
 import os
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ def home():
     if request.method == 'POST':
         query_str = request.form.get('searchbox')
         doc_list = plc_search.search(query_str)
-        result_list = [plc_show.get_item(doc_item[0]) for doc_item in doc_list[0:max_display]]
+        result_list = [topic_model_service.map_topic_string(plc_show.get_item(doc_item[0])) for doc_item in doc_list[0:max_display]]
         return render_template('result.html', querystr=query_str, resultlist=result_list, resultlen=len(result_list))
     return render_template('index.html')
 
@@ -30,6 +31,7 @@ if __name__ == '__main__':
     plc_search.initialize()
     plc_show = PlaceShow(db_config)
     plc_show.load()
+    topic_model_service = TopicModelService('./backend/topicModel/model/model', './backend/topicModel/preprocessed_reviews.txt', './backend/topicModel/topicInvertedIndex.json')
     max_display = 20
 
     app.run(debug=True, port=5000)
--- indexing/backend/topicModel/TopicModelService.py
+++ indexing/backend/topicModel/TopicModelService.py
@@ -11,6 +11,48 @@ class TopicModelService:
         self.dictionary = gensim.corpora.Dictionary(self.model_corpus)
         self.lda_model = gensim.models.LdaModel.load(model_path)
         self.topicInvertedIndex = JsonIO.read_json_file(inverted_index_path)
+        self.topic_string_dic = {
+            0: 'fun', 
+            1: 'customer service',
+            2: 'upscale',
+            3: 'art',
+            4: 'tourism',
+            5: 'animals',
+            6: 'concert',
+            7: 'night life',
+            8: 'beach',
+            9: 'great people',
+            10: 'event hosting',
+            11: 'casino', 
+            12: 'friendly staff',
+            13: 'homely',
+            14: 'nature',
+            15: 'money',
+            16: 'airport',
+            17: 'family',
+            18: 'location',
+            19: 'soul food',
+            20: 'party food',
+            21: 'transportation',
+            22: 'cool',
+            23: 'relaxing',
+            24: 'forest',
+            25: 'price',
+            26: 'breakfast',
+            27: 'shopping',
+            28: 'wait',
+            29: 'at home feel',
+            30: 'hotel',
+            31: 'good service',
+            32: 'parking',
+            33: 'history',
+            34: 'car service',
+            35: 'amazing staff',
+            36: 'beautiful view',
+            37: 'swimming',
+            38: 'automobile',
+            39: 'old timey'
+        }
         
     def get_query_topic(self, query):
         """Classifies Query to Topic in Model"""
@@ -52,4 +94,9 @@ class TopicModelService:
         if doc_id in topic_docs:
             topic_model_score += topic_docs[doc_id]
         return topic_model_score
+
+    def map_topic_string(self, place):
+        for placeTopic in place['review_topics']:
+            placeTopic['topic'] = self.topic_string_dic[placeTopic['topic']]
+        return place
     
